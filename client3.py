80f94dcd3f3db308ab2250595def4de650c54945
Ibrahim Alghamdi <39561810+toomanaa@users.noreply.github.com>
2020-08-18 21:48:10 +0300

    Add files via upload

--- client3.py
+++ client3.py
@@ -1,17 +1,25 @@
 import socket
+import time 
 s=socket.socket()
 host=socket.gethostname()
 port=8080
 
-a=int(input("Enter a number) "))
-
-b=int(input("Enter a number) "))
 
 
 
 s.connect((host, port))
-s.sendall(str.encode("\n".join([str(a), str(b)])))
+averageServerUtilization = float(s.recv(2048).decode('utf-8'))
+print("CPU utilization is %d"%averageServerUtilization)
+if(averageServerUtilization) <=10:
+    start_time = time.time()
+    a=int(input("Enter a number) "))
 
-m=int((s.recv(2048)).decode('utf-8'))
-print("Recieved sum is %d"%m)
+    b=int(input("Enter a number) "))
+    s.sendall(str.encode("\n".join([str(a), str(b)])))
+
+    m=int((s.recv(2048)).decode('utf-8'))
+    print("Recieved sum is %d"%m)
+    print("--- %s seconds ---" % (time.time() - start_time))
+else:
+    print("CPU is  high with %d"%averageServerUtilization)
 s.close
--- server3.py
+++ server3.py
@@ -1,6 +1,7 @@
 import socket
 import psutil
 import platform
+import time
 from datetime import datetime
 s=socket.socket()
 host=socket.gethostname()
@@ -9,9 +10,13 @@ s.bind((host,port))
 s.listen(5)
 while 1:
     c,address=s.accept()
+    averageServerUtilization = psutil.cpu_percent()
+    c.send(str.encode(str(averageServerUtilization)))
+    start_time = time.time()
     a, b = [int(i) for i in c.recv(2048).decode('utf-8').split('\n')]
     print("Recieved two  from client is n=%d,e=%d\n"%(a,b))
     su=a*b
     print("sum is %d\n"%su)
     c.send(str.encode(str(su)))
+    print("--- %s seconds ---" % (time.time() - start_time))
 s.close
