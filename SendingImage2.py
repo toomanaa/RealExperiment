9ac87fce3921de5f4175107c0669e3648ae6bd45
Ibrahim Alghamdi <39561810+toomanaa@users.noreply.github.com>
2020-09-10 11:25:50 +0100

    Add files via upload

--- SendingImage2.py
+++ SendingImage2.py
@@ -3,90 +3,127 @@ import socket
 import sys
 import time
 import struct
+from statistics import mean
 
 
-# host="192.168.1.3"
+# host="0.0.0.0"
 host="172.20.10.13"
 port=40000
 
 # Now we can create socket object
-s = socket.socket()
+
 
 # Lets connect to that port where server may be running
-s.connect((host, port))
+processingTime = []
 
-# averageServerUtilization = float(s.recv(2048).decode('utf-8'))
-
-unpacker = struct.Struct('f')
-
-data = s.recv(unpacker.size)
-
-averageServerUtilization1 = struct.unpack('f', data )
-# print('received {!r}'.format(binascii.hexlify(averageServerUtilization1)))
-averageServerUtilization1 = float('.'.join(str(ele) for ele in averageServerUtilization1)) 
-averageServerUtilization1  = "{:.3f}".format(averageServerUtilization1)
-print(type(averageServerUtilization1))
-averageServerUtilization1 = float(averageServerUtilization1)
-print("CPU utilization is ", averageServerUtilization1)
-print(type(averageServerUtilization1))
-
-
-unpacker = struct.Struct('f')
-
-data2 = s.recv(unpacker.size)
-
-averageServerUtilization2 = struct.unpack('f', data2 )
-# print('received {!r}'.format(binascii.hexlify(averageServerUtilization1)))
-averageServerUtilization2 = float('.'.join(str(ele) for ele in averageServerUtilization2)) 
-averageServerUtilization2  = "{:.3f}".format(averageServerUtilization2)
-print(type(averageServerUtilization2))
-averageServerUtilization2 = float(averageServerUtilization2)
-print("CPU utilization is ", averageServerUtilization2)
-print(type(averageServerUtilization2))
-
+for number in range(5):
+    # averageServerUtilization = float(s.recv(2048).decode('utf-8'))
+    time.sleep(10)
+    s = socket.socket()
+    s.connect((host, port))
     
-# s.shutdown(socket.SHUT_WR)
+    unpacker = struct.Struct('f')
+    data = s.recv(unpacker.size)
 
-# averageServerUtilization2 = float(s.recv(2048).decode('utf-8'))
+    averageServerUtilization1 = struct.unpack('f', data )
+    # print('received {!r}'.format(binascii.hexlify(averageServerUtilization1)))
+    averageServerUtilization1 = float('.'.join(str(ele) for ele in averageServerUtilization1)) 
+    averageServerUtilization1  = "{:.3f}".format(averageServerUtilization1)
+    print(type(averageServerUtilization1))
+    averageServerUtilization1 = float(averageServerUtilization1)
+    print("CPU utilization is ", averageServerUtilization1)
+    print(type(averageServerUtilization1))
 
-# print("CPU utilization is %d"%averageServerUtilization2)
 
-# s.shutdown(socket.SHUT_WR)
+    unpacker = struct.Struct('f')
 
-baseLine = min(averageServerUtilization1,averageServerUtilization2)
+    data2 = s.recv(unpacker.size)
 
-# averageServerUtilization3 = float(s.recv(2048).decode('utf-8'))
+    averageServerUtilization2 = struct.unpack('f', data2 )
+    # print('received {!r}'.format(binascii.hexlify(averageServerUtilization1)))
+    averageServerUtilization2 = float('.'.join(str(ele) for ele in averageServerUtilization2)) 
+    averageServerUtilization2  = "{:.3f}".format(averageServerUtilization2)
+    print(type(averageServerUtilization2))
+    averageServerUtilization2 = float(averageServerUtilization2)
+    print("CPU utilization is ", averageServerUtilization2)
+    print(type(averageServerUtilization2))
 
-# print("CPU utilization is %d"%averageServerUtilization3)
 
-# s.shutdown(socket.SHUT_WR)
-# # s.shutdown(socket.SHUT_WR)
+    unpacker = struct.Struct('f')
 
-if(averageServerUtilization2 <= 60):
-    start_time = time.time()
-    # We can send file sample.txt
-    file = open("plane.jpeg", "rb")
-    SendData = file.read(60000)
+    data3 = s.recv(unpacker.size)
 
-    
+    averageServerUtilization3 = struct.unpack('f', data3 )
+    # print('received {!r}'.format(binascii.hexlify(averageServerUtilization1)))
+    averageServerUtilization3 = float('.'.join(str(ele) for ele in averageServerUtilization3)) 
+    averageServerUtilization3  = "{:.3f}".format(averageServerUtilization3)
+    print(type(averageServerUtilization3))
+    averageServerUtilization3 = float(averageServerUtilization3)
+    print("CPU utilization is ", averageServerUtilization3)
+    print(type(averageServerUtilization3))
 
-    while SendData:
-        # Now we can receive data from server
-        #Now send the content of sample.txt to server
-         # Now we can receive data from server
-        print("\n\n################## Below message is received from server ################## \n\n ", s.recv(1024).decode("utf-8"))
-        s.send(SendData)
+
+#     unpacker = struct.Struct('f')
+
+#     data4 = s.recv(unpacker.size)
+
+#     averageServerUtilization4 = struct.unpack('f', data4 )
+    # print('received {!r}'.format(binascii.hexlify(averageServerUtilization1)))
+#     averageServerUtilization4 = float('.'.join(str(ele) for ele in averageServerUtilization4)) 
+#     averageServerUtilization4  = "{:.3f}".format(averageServerUtilization4)
+#     print(type(averageServerUtilization4))
+#     averageServerUtilization4 = float(averageServerUtilization4)
+#     print("Memory ", averageServerUtilization4)
+#     print(type(averageServerUtilization4))
+
+    baseLine = min(averageServerUtilization1,averageServerUtilization2)
+
+
+
+    if(averageServerUtilization3 <= baseLine):
+        start_time = time.time()
+        # We can send file sample.txt
+        file = open("sheep.jpeg", "rb")
         SendData = file.read(60000)
-        s.shutdown(socket.SHUT_WR)
-        print("The prediction results")
-        print(s.recv(1024).decode())
+
+        while SendData:
+            # Now we can receive data from server
+            #Now send the content of sample.txt to server
+            # Now we can receive data from server
+            print("\n\n################## Below message is received from server ################## \n\n ", s.recv(1024).decode("utf-8"))
+            s.send(SendData)
+            SendData = file.read(60000)
+            s.shutdown(socket.SHUT_WR)
+            print("The prediction results")
+            print(s.recv(1024).decode())
     
+        processingTime.append(time.time() - start_time)
+        print("--- %s seconds ---" % (time.time() - start_time))
+        # Close the connection from client side
+        s.close()
+    else:
+        time.sleep(5)
+        start_time = time.time()
+        # We can send file sample.txt
+        file = open("sheep.jpeg", "rb")
+        SendData = file.read(60000)
 
-    print("--- %s seconds ---" % (time.time() - start_time))
-    # Close the connection from client side
-    s.close()
+        while SendData:
+            # Now we can receive data from server
+            #Now send the content of sample.txt to server
+            # Now we can receive data from server
+            print("\n\n################## Below message is received from server ################## \n\n ", s.recv(1024).decode("utf-8"))
+            s.send(SendData)
+            SendData = file.read(60000)
+            s.shutdown(socket.SHUT_WR)
+            print("The prediction results")
+            print(s.recv(1024).decode())
+    
+        processingTime.append(time.time() - start_time)
+        print("--- %s seconds ---" % (time.time() - start_time))
+        # Close the connection from client side
+        s.close()
+        print("The cpu is too high")
+        s.close()
 
-else:
-    print("The cpu is too high")
-    s.close()
-
+print(mean(processingTime))
